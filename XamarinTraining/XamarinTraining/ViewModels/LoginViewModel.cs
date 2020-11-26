5f106ebb00c76b4758ed2efacf4aaabd89800aa7
hongyee.gow@avanade.com <hongyee.gow@avanade.com>
2020-11-26 16:09:09 +0800

    Add project files.

---
+++ XamarinTraining/XamarinTraining/ViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using XamarinTraining.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XamarinTraining.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public Command LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(OnLoginClicked);
+        }
+
+        private async void OnLoginClicked(object obj)
+        {
+            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+        }
+    }
+}
